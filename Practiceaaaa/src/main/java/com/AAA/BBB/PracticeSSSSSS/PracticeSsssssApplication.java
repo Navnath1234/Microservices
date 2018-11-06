702e7d09539e6d4e00726cebed753e11271b0c5a
Navnath1234 <44070050+Navnath1234@users.noreply.github.com>
2018-11-07 01:00:01 +0530

    Add files via upload

---
+++ Practiceaaaa/src/main/java/com/AAA/BBB/PracticeSSSSSS/PracticeSsssssApplication.java
@@ -0,0 +1,13 @@
+package com.AAA.BBB.PracticeSSSSSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSsssssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSsssssApplication.class, args);
+
+	}
+}
